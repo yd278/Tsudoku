40815ff1aa02b07dd7af741b40c9f6213f516ce1
Y. Ding <argentumdyz@gmail.com>
2024-04-19 19:54:19 -0700

    refactor engineAPI

--- src/engine/engineAPI.cpp
+++ src/engine/engineAPI.cpp
@@ -1,5 +1,5 @@
 #include <napi.h>
-#include "engine.h"
+#include "Grid.h"
 
 Napi::String GenerateWrapped(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
@@ -11,21 +11,24 @@ Napi::String GenerateWrapped(const Napi::CallbackInfo& info) {
     }
 
     int difficulty = info[0].As<Napi::Number>().Int32Value();
-    std::string result = generate(difficulty);  // Call the C++ function
+    Grid grid(difficulty);
+    std::string result =grid.toString();  // Call the C++ function
     return Napi::String::New(env, result);
 }
 
-Napi::String FindNextStepWrapped(const Napi::CallbackInfo& info) {
+Napi::Buffer<uint8_t> FindNextStepWrapped(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
-
     if (info.Length() != 1 || !info[0].IsString()) {
         Napi::TypeError::New(env, "String expected").ThrowAsJavaScriptException();
-        return Napi::String::New(env, "");  // Return an empty string on error
+        return Napi::Buffer<uint8_t>::New(env, 0);  // 返回一个空的buffer
     }
 
-    std::string grid = info[0].As<Napi::String>().Utf8Value();
-    std::string result = findNextStep(grid);  // Call the C++ function
-    return Napi::String::New(env, result);
+    std::string gridPattern = info[0].As<Napi::String>().Utf8Value();
+    Grid grid(gridPattern);
+    std::vector<uint8_t>& result = grid.nextStep();  // 调用C++函数获取引用
+
+    // 使用结果数据创建Buffer，这里数据会被复制到Node.js的Buffer中
+    return Napi::Buffer<uint8_t>::Copy(env, result.data(), result.size());
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
