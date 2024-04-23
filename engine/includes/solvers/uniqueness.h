2a43b24a677e825f9ed876064fca79b06d6e0888
Y. Ding <argentumdyz@gmail.com>
2024-04-22 21:59:25 -0700

    uniqueness test cases

---
+++ engine/includes/solvers/uniqueness.h
@@ -0,0 +1,14 @@
+#ifndef UNIQUENESS_H
+#define UNIQUENESS_H
+
+#include "Grid.h"
+void uniquenessTestType1(Grid &grid);
+void uniquenessTestType2(Grid &grid);
+void uniquenessTestType3(Grid &grid);
+void uniquenessTestType4(Grid &grid);
+void uniquenessTestType5(Grid &grid);
+void findHiddenRectangle(Grid &grid);
+void avoidableRectangle1(Grid &grid);
+void avoidableRectangle2(Grid &grid);
+void bivalueUniversalGraveP1(Grid &grid);
+#endif  // UNIQUENESS_H
