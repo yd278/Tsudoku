6d5a5c1cc362d8c8eafd535b8a6c1b3ae0a1cf9c
Y. Ding <argentumdyz@gmail.com>
2024-05-08 17:17:15 -0700

    single cell

---
+++ src/components/main-grid/single-cell-wrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import singleCellPen from './single-cell-pen.vue';
+import singleCellPencil from './single-cell-pencil.vue';
+defineProps<{
+    isPencil: boolean,
+    candidates: boolean[],
+    reverseY:boolean,
+    isGiven:boolean,
+    value:number
+}>()
+</script>
+
+<template>
+<singleCellPencil v-if="isPencil", :candidates="candidates" :reverse-y="reverseY"></singleCellPencil>
+<singleCellPen v-else :is-given="isGiven" :value="value"></singleCellPen>
+<style scoped>
+</style>
